7cd3443c167720cfffaac5870be9500f2420298b
itsvectooor <vitormoura288@gmail.com>
2025-03-11 00:06:01 +0000

    criei um jogo de Super Trunfo com 2 cartas

---
+++ superTrunfo.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+ 
+int main() {
+    int pop;
+    float area;
+    float pib;
+    int turismo;
+
+    printf("Vamos cadastrar a carta A01!\n");
+    printf("Digite a população da cidade:");
+    scanf("%i", &pop);
+    printf("Agora, a área:");
+    scanf("%3f", &area);
+    printf("Seu PIB:");
+    scanf("%3f", &pib);
+    printf("E, por fim, quantos pontos turísticos essa cidade tem:");
+    scanf("%i", &turismo);
+    printf("As informações cadastradas foram:\n");
+    printf("Código da carta: A01.\n" "População:%i\n",pop);
+    printf( "Área:%f\n",area);
+    printf("PIB:%f\n", pib);
+    printf("Pontos turisticos:%i\n", turismo);
+
+    
+    printf("Agora vamos cadastrar a carta A02:\n");
+    printf("Digite a população da cidade:");
+    scanf("%i", &pop);
+    printf("Agora, a área:");
+    scanf("%f", &area);
+    printf("Seu PIB:");
+    scanf("%f", &pib);
+    printf("E, por fim, quantos pontos turísticos essa cidade tem:");
+    scanf("%i", &turismo);
+    printf("As informações cadastradas foram:\n");
+    printf("Código da carta: A02.\n" "População:%i\n",pop);
+    printf( "Área:%f\n",area);
+    printf("PIB:%f\n", pib);
+    printf("Pontos turisticos:%i\n", turismo);
+    
+
+}
+ 
+    
